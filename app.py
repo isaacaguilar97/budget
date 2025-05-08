df208e4da15526f42cfcb6cedfa6e0f77e496514
Isaac Aguilar <aguilaris203@gmail.com>
2025-05-08 00:30:48 -0700

    Finished App

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 #%% Import libraries
 import pandas as pd
+import gspread
 import streamlit as st
-import plotly.express as px
+import plotly.graph_objects as go
+from oauth2client.service_account import ServiceAccountCredentials
 from streamlit_gsheets import GSheetsConnection
 from datetime import datetime
 
@@ -69,7 +71,30 @@ final_df = pd.DataFrame({
     'Balance': budget[td] - actuals[td],
     'Budget': budget[td]
 })
-final_df
+
+# Parameters
+col_inter = ['Food', 'Supplies', 'Self Care', 'Isaac', 'Adri', 'Eliam', 'Dates/Fun', 'Gifts', 'Fast Food', 'Junk Food']
+filtered_df = final_df[final_df['Category'].isin(col_inter)]
+
+# Authenticate and connect to Google Sheets
+def connect_to_gsheet(creds_dict, spreadsheet_name, sheet_name):
+    scope = ["https://spreadsheets.google.com/feeds", 
+             'https://www.googleapis.com/auth/spreadsheets',
+             "https://www.googleapis.com/auth/drive.file", 
+             "https://www.googleapis.com/auth/drive"]
+    
+    credentials = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scope)
+    client = gspread.authorize(credentials)
+    spreadsheet = client.open(spreadsheet_name)  
+    return spreadsheet.worksheet(sheet_name)  # Access specific sheet by name
+
+# Google Sheet credentials and details
+SPREADSHEET_NAME = 'Cash Flow Fam Aguilar'
+SHEET_NAME = 'Real'
+CREDENTIALS_FILE = st.secrets["google"]
+
+# Connect to the Google Sheet
+sheet_by_name = connect_to_gsheet(CREDENTIALS_FILE, SPREADSHEET_NAME, sheet_name=SHEET_NAME)
 
 #%% Start Streamlit app
 
@@ -78,19 +103,78 @@ with st.sidebar:
     # App Title
     st.title('Enter your Transaction')
 
-    # Position Input
-    pos1 = st.selectbox("Select Position", final_df['pos'].unique().tolist())
+    with st.form(key="Enter your Transaction"):
+        amt = st.number_input("Amount")
+        cty = st.selectbox("Category", col_inter)
+        card = st.selectbox("Payment Method", ['Silver', 'Barclays', 'DFCU', 'Venture', 'Cosco', 'Chase'])
+        # Submit button inside the form
+        submitted = st.form_submit_button("Submit")
+        # Handle form submission
+        if submitted:
+            if amt and cty and card:  # Basic validation to check if required fields are filled
+                # Update Spreadsheet
+                # Get the values of row 5 and column B
+                row_5 = sheet_by_name.row_values(5)
+                col_B = sheet_by_name.col_values(2)
 
-    # Height unit
-    h = st.selectbox("Select Height Unit", ['inches', 'meters'])
-    unit_dic = {'inches': 'height_in', 'meters': 'height_m'}
+                # Find the column and row indexes
+                col_index = row_5.index(td) + 1
+                row_index = col_B.index(cty) + 1
 
-    # Warning message
-    st.info("Be aware that results come from a sample of NBA players from season 2023")
+                # Read current value
+                current_value = sheet_by_name.cell(row_index, col_index).value
+                new_value = float(current_value or 0) + 10
 
-    # Abbreviations
-    with st.expander("Abbreviation Index"):
-        st.table(abb)
+                # Update Category
+                sheet_by_name.update_cell(row_index, col_index, new_value)
+
+                if card is not "Chase": # I don't make updates if I paid with my chase
+                    # Update Debt
+                    sheet_by_name.update_cell(row_index, col_index, new_value)
+                    row_index = col_B.index("Deuda") + 1
+                    sheet_by_name.update_cell(row_index, col_index, float(sheet_by_name.cell(row_index, col_index).value)+10)
+
+                    # Update Card
+                    row_index = col_B.index(card) + 1
+                    sheet_by_name.update_cell(row_index, col_index, float(sheet_by_name.cell(row_index, col_index).value)+10)
+
+                st.success("Data added successfully!")
+            else:
+                st.error("Please fill out the form correctly.")
 
 # Header 1
-st.header('Explore the Positions')
+st.header('Family Aguilar Budget')
+
+st.divider()
+
+for i in range(filtered_df.shape[0]):
+    cat = filtered_df['Category'].iloc[i]
+    but = filtered_df['Budget'].iloc[i]
+    bal = filtered_df['Balance'].iloc[i]
+    remaining_pct = bal/but
+    bar_col = 'red' if remaining_pct <= 0 else 'orange' if remaining_pct <= 0.15 else 'green'
+
+    fig = go.Figure(go.Indicator(
+        mode = "gauge,delta", 
+        value = but - bal,
+        domain = {'x': [0.15, 1], 'y': [0, 1]},
+        title = {'text' :f"<b>{cat}</b>"},
+        delta = {'reference': but - 2*bal, 
+                 "valueformat": "$.0f",
+                 "increasing": {"color": "white", "symbol": ""}, 
+                 "decreasing": {"color": "white", "symbol": ""}},
+        gauge = {
+            'shape': "bullet",
+            'axis': {'range': [None, but*1.20]},
+            'bar': {'color': bar_col},
+            'threshold': {
+                'line': {'color': "red", 'width': 2},
+                'thickness': 0.75,
+                'value': but}
+            }))
+    fig.update_layout(
+        height = 60,
+        margin=dict(t=0, b=0),
+        autosize=True 
+        )
+    st.plotly_chart(fig)
